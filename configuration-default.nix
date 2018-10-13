e25d4337fad17f47ed92c801545c15fa560bfaf0
Bobby Rong <rjl931189261@126.com>
2018-10-13 13:15:11 +0000

    Update configuration-default.nix

--- configuration-default.nix
+++ configuration-default.nix
@@ -1,41 +1,44 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-                                                                                                                                                                                                                                             
-{ config, pkgs, ... }:                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                             
-{                                                                                                                                                                                                                                            
-  imports =                                                                                                                                                                                                                                  
-    [ # Include the results of the hardware scan.                                                                                                                                                                                            
-      ./hardware-configuration.nix                                                                                                                                                                                                           
-    ];                                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                             
-  # Use the systemd-boot EFI boot loader.                                                                                                                                                                                                    
-  boot.loader.systemd-boot.enable = true;                                                                                                                                                                                                    
-  boot.loader.efi.canTouchEfiVariables = true;                                                                                                                                                                                               
-                                                                                                                                                                                                                                             
-  # networking.hostName = "nixos"; # Define your hostname.                                                                                                                                                                                   
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.                                                                                                                                                       
-                                                                                                                                                                                                                                             
-  # Select internationalisation properties.                                                                                                                                                                                                  
-  # i18n = {                                                                                                                                                                                                                                 
-  #   consoleFont = "Lat2-Terminus16";                                                                                                                                                                                                       
-  #   consoleKeyMap = "us";                                                                                                                                                                                                                  
-  #   defaultLocale = "en_US.UTF-8";                                                                                                                                                                                                         
-  # };                                                                                                                                                                                                                                       
-                                                                                                                                                                                                                                             
-  # Set your time zone.                                                                                                                                                                                                                      
-  # time.timeZone = "Europe/Amsterdam";                                                                                                                                                                                                      
-                                                                                                                                                                                                                                             
-  # List packages installed in system profile. To search, run:                                                                                                                                                                               
-  # $ nix search wget                                                                                                                                                                                                                        
-  # environment.systemPackages = with pkgs; [                                                                                                                                                                                                
-  #   wget vim                                                                                                                                                                                                                               
-  # ];                                                                                                                                                                                                                                       
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
@@ -70,7 +73,7 @@
   # services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
+  # users.users.guest = {
   #   isNormalUser = true;
   #   uid = 1000;
   # };
@@ -79,6 +82,6 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system.stateVersion = "18.09"; # Did you read the comment?
 
 }
